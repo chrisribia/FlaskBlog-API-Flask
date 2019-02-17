27adfa85f4b72156b9ed3723a28f572e291a28a7
chrisribia <chrisribia@gmail.com>
2019-02-17 16:27:20 +0300

    add version2 routes

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,8 @@
 
 from flask import Blueprint, Flask
 from app.api.v1.routes import VERSION_ONE as v1
+from app.api.v2.routes import VERSION_DOS as v2
+from app.api.db_config import create_tables,destroy_tables
 
 
 from app.instance.config import APP_CONFIG
@@ -11,8 +13,13 @@ def create_app(config_name):
     app.config '''
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(APP_CONFIG[config_name])# Load the default configuration
-    app.url_map.strict_slashes = False   
+    app.url_map.strict_slashes = False 
+    
+    destroy_tables()
+    create_tables()
+    
     app.register_blueprint(v1)
+    app.register_blueprint(v2)
 
     return app
     
